dbe3104c1189e2a468b795441e1c3220118671bf
OldMemory <35580066700@qq.com>
2025-04-02 19:59:52 +0800

    2025_04_02 19:59

--- operation_create_fake_signature.py
+++ operation_create_fake_signature.py
@@ -13,7 +13,7 @@ def create_fake_signature(input_file,output_file):
     """
     signature = random.choice(all_signatures)
     #print(signature)
-    log1 = "D:\\毕业设计\\example1\\logs\\%s" % (time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
+    log1 = "D:\\毕业设计\\example1\\logs\\%s-%s" % ("FakeSignatureProcess",time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
     log1 = log1+".log"
     command = "python D:\\毕业设计\\example1\\sigthief.py -s %s -t %s -o %s > %s" % (signature,input_file,output_file,log1)
     #print(command)
--- operation_upx_encryption.py
+++ operation_upx_encryption.py
@@ -1,7 +1,13 @@
-def upx_encryption(input_file):
-    # upx加壳直接处理目标文件
-    command = "D:\\毕业设计\\example1\\upx_shell\\upx.exe "
+import os
+import time
 
+def upx_encryption(input_file):
+    # upx加壳直接处理目标文件 处理后的文件位于目标路径
+    log1 = "D:\\毕业设计\\example1\\logs\\%s-%s" % (
+    "UPX_Process", time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
+    log1 = log1 + ".log"
+    command = "D:\\毕业设计\\example1\\upx_shell\\upx.exe %s > %s" %(input_file,log1)
+    os.system(command)
 
 if __name__ == "__main__":
-    pass
+    upx_encryption(r"D:\毕业设计\example1\upx_shell\HelloWorld1.exe")
