67a57f2797d4fe0280fe422a224ad4ad0551638e
OldMemory <35580066700@qq.com>
2025-04-07 10:53:34 +0800

    2025_04_07 10:53

---
+++ operation_modify_checksum.py
@@ -0,0 +1,26 @@
+import pefile
+import time
+def modify_pe_checksum(input_file, output_file, new_checksum=None):
+    # 加载PE文件
+    pe = pefile.PE(input_file)
+
+    # 获取原始校验和
+    original_checksum = pe.OPTIONAL_HEADER.CheckSum
+
+    # 设置新时间戳（如果未提供，使用当前时间）
+    if new_checksum is None:
+        new_checksum = 0
+    pe.OPTIONAL_HEADER.CheckSum = new_checksum
+    """
+
+    # 保存修改后的文件
+    """
+    pe.write(output_file)
+    pe.close()
+    """
+    print(original_checksum)
+    """
+
+if __name__ == "__main__":
+
+    modify_pe_checksum("HelloWorld2.exe","modifiedHello2.exe",0)
