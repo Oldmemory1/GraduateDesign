57ef9a91a5af26ad4f9e814f77e0e92a9f3f4ae3
OldMemory <35580066700@qq.com>
2025-04-09 16:53:00 +0800

    2025_04_09 16:52 add action_modify_optional_header_1.py modify actions_list.py

--- actions/actions_list.py
+++ actions/actions_list.py
@@ -1,9 +1,9 @@
 actions_list=[
-    "create_fake_signature",
-    "insert_useless_data_to_end",
-    "modify_checksum",
-    "modify_timestamp",
-    "rename_random_section",
+    "action_create_fake_signature",
+    "action_insert_useless_data_to_end",
+    "action_modify_checksum",
+    "action_modify_timestamp",
+    "action_rename_random_section",
     # but if we execute this action, other action can not be executed because they will break signature
     "upx_encryption",
     # the encryption can only be executed once
@@ -11,4 +11,5 @@ actions_list=[
     "action_add_bytes_to_section_cave_1",
     "action_add_section_benign_data_1",
     "action_break_optional_header_checksum_1",
+    "action_modify_optional_header_1"
 ]
