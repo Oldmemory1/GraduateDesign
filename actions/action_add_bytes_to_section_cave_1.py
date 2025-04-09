24941ff2934de3521744c20f58be3fbcd9b39849
OldMemory <35580066700@qq.com>
2025-04-09 16:23:35 +0800

    2025_04_09 16:23 add action_add_bytes_to_section_cave_1.py

--- actions/action_add_bytes_to_section_cave_1.py
+++ actions/action_add_bytes_to_section_cave_1.py
@@ -5,8 +5,7 @@ import time
 
 from init.reset_test_enviroment import init_environment
 from operation_modules.copy_file import copy_file
-from operation_modules.process_remaster.operation_add_benign_data_overlay_1 import append_benign_data_overlay
-from string_generator.generated_strings.get_a_random_string_from_strings import get_random_string
+from operation_modules.process_remaster.operation_add_bytes_to_section_cave_1 import add_bytes_to_section_cave
 from string_generator.generated_strings.strings_length_4096 import strings_4096bytes
 
 logging.basicConfig(
@@ -16,21 +15,20 @@ logging.basicConfig(
     filename=r"D:\毕业设计\example1\logs\record.log",        # 输出到文件（不指定则默认输出到控制台）
     filemode="a"               # 文件写入模式（'a' 追加，'w' 覆盖）
 )
-def action_add_benign_data_overlay_1(input_file_,appended_data_,enable_log = False):
-    # use for testing/debugging actions
+def action_add_bytes_to_section_cave_1(input_file_,string_list_,enable_log = False):
     with open(input_file_, "rb") as f1:
         bytez = f1.read()
     if enable_log:
         m1 = hashlib.sha256()
         m1.update(bytez)
-        logging.info("action_add_benign_data_overlay_1 before file:" + m1.hexdigest())
+        logging.info("action_add_bytes_to_section_cave_1 before file:" + m1.hexdigest())
 
 
-    bytez1 = append_benign_data_overlay(bytez, appended_data=appended_data_)
+    bytez1 = add_bytes_to_section_cave(bytez, string_list=string_list_)
 
     file_name = os.path.basename(input_file_)
     temp_name = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
-    temp_name = temp_name + "-action_add_benign_data_overlay_1-" + file_name
+    temp_name = temp_name + "-action_add_bytes_to_section_cave_1-" + file_name
     address1 = r"D:\毕业设计\example1\operation_modules\process_remaster\example_debug\temp" + "\\" + temp_name
 
     with open(address1, "wb+") as f2:
@@ -42,10 +40,10 @@ def action_add_benign_data_overlay_1(input_file_,appended_data_,enable_log = Fal
     if enable_log:
         m2 = hashlib.sha256()
         m2.update(bytez1)
-        logging.info("action_add_benign_data_overlay_1 after file:" + m2.hexdigest())
+        logging.info("action_add_bytes_to_section_cave_1 after file:" + m2.hexdigest())
 if __name__ == "__main__":
         init_environment()
         input_file = r"D:\毕业设计\example1\source_file\sample1.exe"
-        action_add_benign_data_overlay_1(input_file_=input_file,
-                                         appended_data_=get_random_string(string_list=strings_4096bytes),
-                                         enable_log=True)
+        action_add_bytes_to_section_cave_1(input_file_=input_file,
+                                           string_list_=strings_4096bytes,
+                                           enable_log=True)
--- operation_modules/process_remaster/example_debug/dump_temp.py
+++ operation_modules/process_remaster/example_debug/dump_temp.py
@@ -1,6 +1,6 @@
 from init.clear_directory import clear_directory
 
-
+# 清空temp文件夹里的暂存内容
 def dump_temp():
     clear_directory(target_dir="./temp")
 if __name__=="__main__":
