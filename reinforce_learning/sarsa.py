13422e437f4bc083206e555548d18d091916ee18
OldMemory <35580066700@qq.com>
2025-04-10 09:01:38 +0800

    2025_04_10 9:01

--- reinforce_learning/sarsa.py
+++ reinforce_learning/sarsa.py
@@ -2,11 +2,23 @@ import os
 
 import numpy as np
 
+from actions.action_add_benign_data_overlay_1 import action_add_benign_data_overlay_1
+from actions.action_add_bytes_to_section_cave_1 import action_add_bytes_to_section_cave_1
+from actions.action_add_section_benign_data_1 import action_add_section_benign_data_1
+from actions.action_break_optional_header_checksum_1 import action_break_optional_header_checksum_1
 from actions.action_create_fake_signature import action_create_fake_signature
 from actions.action_insert_useless_data_to_end import action_insert_useless_data_to_end
 from actions.action_modify_checksum import action_modify_checksum
+from actions.action_modify_optional_header_1 import action_modify_optional_header_1
 from actions.action_modify_timestamp import action_modify_timestamp
+from actions.action_modify_timestamp_1 import action_modify_timestamp_1
+from actions.action_remove_debug import action_remove_debug
+from actions.action_rename_random_section import action_rename_random_section
+from actions.action_rename_section_1 import action_rename_section_1
+from actions.action_upx_encryption import action_upx_encryption
+from actions.actions_list import get_random_actions
 from operation_modules.findAllSignatures import get_all_file_paths
+from string_generator.generated_strings.get_a_random_string_from_strings import get_random_string
 from string_generator.generated_strings.strings_length_4096 import strings_4096bytes
 from virus_scanner_module.clamScanner import clamScanner
 
@@ -34,11 +46,100 @@ class SARSA:
         else:
             target = reward + self.gamma * self.q_table[next_state, next_action]
         self.q_table[state, action] += self.alpha * (target - current_q)
+    def train(self,train_dataset_dir,enable_log_):
+        episodes = get_all_file_paths(target_dir=train_dataset_dir)
+        for episode in episodes:
+            state = 0  # 初始状态（未成功次数为0）
+            count = 0
+            done = False
 
+            action = self.choose_action(state)
+            action_name = self.actions_list[action]
+            if action_name == "action_create_fake_signature":
+                action_create_fake_signature(input_file_=episode,
+                                             enable_log=enable_log_)
+            elif action_name == "action_insert_useless_data_to_end":
+                action_insert_useless_data_to_end(input_file_=episode,
+                                                  string_list_=strings_4096bytes,
+                                                  enable_log=enable_log_)
+            elif action_name == "action_modify_checksum":
+                action_modify_checksum(input_file_=episode, new_checksum_=None, enable_log=enable_log_)
+            elif action_name == "action_modify_timestamp":
+                action_modify_timestamp(input_file_=episode, enable_log=enable_log_, new_timestamp_=None)
+            elif action_name == "action_rename_random_section":
+                action_rename_random_section(input_file_=episode, enable_log=enable_log_)
+            elif action_name == "action_upx_encryption":
+                action_upx_encryption(input_file_=episode, enable_log=enable_log_)
+            elif action_name == "action_add_benign_data_overlay_1":
+                action_add_benign_data_overlay_1(input_file_=episode,
+                                                 appended_data_=get_random_string(string_list=strings_4096bytes),
+                                                 enable_log=enable_log_)
+            elif action_name == "action_add_bytes_to_section_cave_1":
+                action_add_bytes_to_section_cave_1(input_file_=episode, string_list_=strings_4096bytes,
+                                                   enable_log=enable_log_)
+            elif action_name == "action_add_section_benign_data_1":
+                action_add_section_benign_data_1(input_file_=episode, string_list_=strings_4096bytes,
+                                                 enable_log=enable_log_)
+            elif action_name == "action_break_optional_header_checksum_1":
+                action_break_optional_header_checksum_1(input_file_=episode, enable_log=enable_log_)
+            elif action_name == "action_modify_optional_header_1":
+                action_modify_optional_header_1(input_file_=episode, enable_log=enable_log_)
+            elif action_name == "action_modify_timestamp_1":
+                action_modify_timestamp_1(input_file_=episode, enable_log=enable_log_)
+            elif action_name == "action_remove_debug":
+                action_remove_debug(input_file_=episode, enable_log=enable_log_)
+            elif action_name == "action_rename_section_1":
+                action_rename_section_1(input_file_=episode, enable_log=enable_log_)
+
+            total_reward = 0
+            origin_file_size = os.path.getsize(episode)
+            while not done:
+                valid = True
+                # 执行动作并获取结果
+                # clamScanner反病毒软件扫描
+                if clamScanner(episode):
+                    valid = False
+                elif not clamScanner(episode):
+                    valid = True
+                if valid:
+                    reward = 100
+                    now_file_size = os.path.getsize(episode)
+                    discount = np.log(now_file_size / origin_file_size)
+                    reward = reward / ((1 / 3) * discount + 1)
+                    print("episode:" + os.path.basename(episode) + "reward:" + str(reward))
+                    next_state = None
+                    done = True
+                else:
+                    count += 1
+                    if count >= 10:
+                        reward = -100
+                        print("episode:" + os.path.basename(episode) + "fail")
+                        next_state = None
+                        done = True
+                    else:
+                        reward = -1
+                        next_state = count
+                        done = False
+
+                total_reward += reward
+
+                # 更新Q表
+                if done:
+                    self.update(state, action, reward, next_state=None, done=True)
+                else:
+                    next_action = self.choose_action(next_state)
+                    self.update(state, action, reward, next_state, next_action)
+                    state = next_state
+                    action = next_action
+
+            # 输出训练进度
+            if (episode + 1) % 100 == 0:
+                print(f"Episode: {episode + 1}, Total Reward: {total_reward}")
 
 
 
 # 训练sarsa 模型 episodes为文件列表
+"""
 def train_sarsa(agent, train_dataset_dir,enable_log_):
     episodes = get_all_file_paths(target_dir=train_dataset_dir)
     for episode in episodes:
@@ -59,7 +160,28 @@ def train_sarsa(agent, train_dataset_dir,enable_log_):
             action_modify_checksum(input_file_=episode,new_checksum_=None,enable_log=enable_log_)
         elif action_name == "action_modify_timestamp":
             action_modify_timestamp(input_file_=episode,enable_log=enable_log_,new_timestamp_=None)
-
+        elif action_name == "action_rename_random_section":
+            action_rename_random_section(input_file_=episode,enable_log=enable_log_)
+        elif action_name == "action_upx_encryption":
+            action_upx_encryption(input_file_=episode,enable_log=enable_log_)
+        elif action_name == "action_add_benign_data_overlay_1":
+            action_add_benign_data_overlay_1(input_file_=episode,
+                                         appended_data_=get_random_string(string_list=strings_4096bytes),
+                                         enable_log=enable_log_)
+        elif action_name == "action_add_bytes_to_section_cave_1":
+            action_add_bytes_to_section_cave_1(input_file_=episode,string_list_=strings_4096bytes,enable_log=enable_log_)
+        elif action_name == "action_add_section_benign_data_1":
+            action_add_section_benign_data_1(input_file_=episode,string_list_=strings_4096bytes,enable_log=enable_log_)
+        elif action_name ==  "action_break_optional_header_checksum_1":
+            action_break_optional_header_checksum_1(input_file_=episode,enable_log=enable_log_)
+        elif action_name == "action_modify_optional_header_1":
+            action_modify_optional_header_1(input_file_=episode,enable_log=enable_log_)
+        elif action_name == "action_modify_timestamp_1":
+            action_modify_timestamp_1(input_file_=episode,enable_log=enable_log_)
+        elif action_name == "action_remove_debug":
+            action_remove_debug(input_file_=episode,enable_log=enable_log_)
+        elif action_name == "action_rename_section_1":
+            action_rename_section_1(input_file_=episode,enable_log=enable_log_)
 
 
 
@@ -78,13 +200,14 @@ def train_sarsa(agent, train_dataset_dir,enable_log_):
                 now_file_size = os.path.getsize(episode)
                 discount = np.log(now_file_size/origin_file_size)
                 reward = reward/((1/3)*discount+1)
-                print("episode:"+os.path.basename(episode)+" reward:"+str(reward))
+                print("episode:"+os.path.basename(episode)+"reward:"+str(reward))
                 next_state = None
                 done = True
             else:
                 count += 1
                 if count >= 10:
                     reward = -100
+                    print("episode:" + os.path.basename(episode) +"fail")
                     next_state = None
                     done = True
                 else:
@@ -108,15 +231,17 @@ def train_sarsa(agent, train_dataset_dir,enable_log_):
             print(f"Episode: {episode + 1}, Total Reward: {total_reward}")
 
 """
-if __name__ == "__main__":
-    n_states = 10  # 状态0-9（对应未成功次数）
-    n_actions = 5  # 可用动作数量
 
-    agent = SARSA(n_states, n_actions)
-    train_sarsa(agent, episodes=1000)
+
+if __name__ == "__main__":
+    n_states_ = 10  # 状态0-9（对应未成功次数）
+    n_actions_ = 5  # 可用动作数量
+    actions_list_ = get_random_actions(n_actions_)
+    agent = SARSA(actions_list=actions_list_,n_states=n_states_, n_actions=n_actions_)
+    #train_sarsa(agent, episodes=1000)
 
     # 打印训练后的Q表
     print("\nTrained Q-table:")
     print(agent.q_table)
 
-"""
+
